ef9b80db64033b6062d9254f22dd062c6305ff9c
Hiroyuki Hasebe <16788901+levi106@users.noreply.github.com>
2022-06-30 00:21:58 +0900

    add sample

---
+++ videoanalyzer/samples/record-live-video/record_locally.py
@@ -0,0 +1,30 @@
+import time
+from videoanalyzer.source import CameraSource
+from videoanalyzer.sink import LocalVideoSink
+from videoanalyzer.pipeline import Pipeline
+from typing import cast
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleSpanProcessor
+)
+
+def main():
+    trace.set_tracer_provider(TracerProvider())
+    provider: TracerProvider = cast(TracerProvider, trace.get_tracer_provider())
+    provider.add_span_processor(
+        SimpleSpanProcessor(ConsoleSpanExporter())
+    )
+
+    source = ('camera', CameraSource(0))
+    sinks = [
+        ('file', 'camera', LocalVideoSink(output_dir="/tmp/"))
+    ]
+    pipeline = Pipeline(source=source, sinks=sinks)
+    pipeline.start()
+    time.sleep(5)
+    pipeline.stop()
+
+if __name__ == "__main__":
+    main()
