5ba7e452855d22975e639a5379719192308f964b
Hiroyuki Hasebe <16788901+levi106@users.noreply.github.com>
2022-07-05 13:17:15 +0900

    add IoTHubMessageSink

---
+++ videoanalyzer/videoanalyzer/sink/iothubmessagesink.py
@@ -0,0 +1,14 @@
+import json
+from ._basesink import BaseSink
+from azure.iot.device import IoTHubModuleClient
+from typing import Any, Dict
+from opentelemetry import trace
+
+class IoTHubMessageSink(BaseSink):
+    def __init__(self, client: IoTHubModuleClient, output_name: str):
+        self._tracer = trace.get_tracer(__name__)
+        self._client = client
+        self._output_name = output_name
+
+    def write(self, frame: Any, props: Dict[str, Any]) -> None:
+        self._client.send_message_to_output(json.dumps(props), self._output_name)
